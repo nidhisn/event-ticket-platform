b3755017adbe91815d84ed6e92223c0c58760707
nidhisn <nidhi24sn@gmail.com>
2026-02-20 15:15:14 +0530

    Configure security, user provisioning filter, and enable JPA auditing

---
+++ src/main/java/com/nidhi/tickets/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.nidhi.tickets.repository;
+
+import com.nidhi.tickets.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
